84f7c4a3b757b1193e8bf11ca0a7adf8eb76e649
Holger Seelig <holger.seelig@yahoo.de>
2015-03-21 06:04:23 +0100

    Added Notification.

--- cobweb.js/cobweb/Browser/Rendering/Gouraud.fs
+++ cobweb.js/cobweb/Browser/Rendering/Gouraud.fs
@@ -1,11 +1,11 @@
 // -*- Mode: C++; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
 precision mediump float;
 
-uniform bool x3d_lighting;        // true if a X3DMaterialNode is attached, otherwise false
-uniform bool x3d_colorMaterial;   // true if a X3DColorNode is attached, otherwise false
+uniform bool x3d_Lighting;        // true if a X3DMaterialNode is attached, otherwise false
+uniform bool x3d_ColorMaterial;   // true if a X3DColorNode is attached, otherwise false
 
-uniform bool      x3d_texturing;  // true if a X3DTexture2DNode is attached, otherwise false
-uniform sampler2D x3d_texture;
+uniform bool      x3d_Texturing;  // true if a X3DTexture2DNode is attached, otherwise false
+uniform sampler2D x3d_Texture;
 
 varying vec4 frontColor; // color
 varying vec4 backColor;  // color
@@ -14,7 +14,7 @@ varying vec4 t;          // texCoord
 vec4
 getTextureColor ()
 {
-	return texture2D (x3d_texture, vec2 (t .s, t .t));
+	return texture2D (x3d_Texture, vec2 (t .s, t .t));
 }
 
 void
@@ -22,23 +22,23 @@ main ()
 {
 	vec4 finalColor = gl_FrontFacing ? frontColor : backColor;
 
-	if (x3d_lighting)
+	if (x3d_Lighting)
 	{
-		if (x3d_texturing)
+		if (x3d_Texturing)
 			finalColor *= getTextureColor ();
 
 		gl_FragColor = finalColor;
 	}
 	else
 	{
-		if (x3d_colorMaterial)
+		if (x3d_ColorMaterial)
 		{
-			if (x3d_texturing)
+			if (x3d_Texturing)
 				finalColor *= getTextureColor ();
 		}
 		else
 		{
-			if (x3d_texturing)
+			if (x3d_Texturing)
 				finalColor = getTextureColor ();
 		}
 
--- cobweb.js/cobweb/Browser/Rendering/Phong.fs
+++ cobweb.js/cobweb/Browser/Rendering/Phong.fs
@@ -1,34 +1,34 @@
 // -*- Mode: C++; coding: utf-8; tab-width: 3; indent-tabs-mode: tab; c-basic-offset: 3 -*-
 precision mediump float;
 
-uniform bool x3d_lighting;      // true if a X3DMaterialNode is attached, otherwise false
-uniform bool x3d_colorMaterial; // true if a X3DColorNode is attached, otherwise false
+uniform bool x3d_Lighting;      // true if a X3DMaterialNode is attached, otherwise false
+uniform bool x3d_ColorMaterial; // true if a X3DColorNode is attached, otherwise false
 
 #define MAX_LIGHTS        8
 #define DIRECTIONAL_LIGHT 0
 #define POINT_LIGHT       1
 #define SPOT_LIGHT        2
-uniform int   x3d_lightType [MAX_LIGHTS]; // 0: DirectionalLight, 1: PointLight, 2: SpotLight
-uniform bool  x3d_lightOn [MAX_LIGHTS];
-uniform vec3  x3d_lightColor [MAX_LIGHTS];
-uniform float x3d_lightIntensity [MAX_LIGHTS];
-uniform float x3d_lightAmbientIntensity [MAX_LIGHTS];
-uniform vec3  x3d_lightAttenuation [MAX_LIGHTS];
-uniform vec3  x3d_lightLocation [MAX_LIGHTS];
-uniform vec3  x3d_lightDirection [MAX_LIGHTS];
-uniform float x3d_lightRadius [MAX_LIGHTS];
-uniform float x3d_lightBeamWidth [MAX_LIGHTS];
-uniform float x3d_lightCutOffAngle [MAX_LIGHTS];
+uniform int   x3d_LightType [MAX_LIGHTS]; // 0: DirectionalLight, 1: PointLight, 2: SpotLight
+uniform bool  x3d_LightOn [MAX_LIGHTS];
+uniform vec3  x3d_LightColor [MAX_LIGHTS];
+uniform float x3d_LightIntensity [MAX_LIGHTS];
+uniform float x3d_LightAmbientIntensity [MAX_LIGHTS];
+uniform vec3  x3d_LightAttenuation [MAX_LIGHTS];
+uniform vec3  x3d_LightLocation [MAX_LIGHTS];
+uniform vec3  x3d_LightDirection [MAX_LIGHTS];
+uniform float x3d_LightRadius [MAX_LIGHTS];
+uniform float x3d_LightBeamWidth [MAX_LIGHTS];
+uniform float x3d_LightCutOffAngle [MAX_LIGHTS];
 
-uniform float x3d_ambientIntensity;
-uniform vec3  x3d_diffuseColor;
-uniform vec3  x3d_specularColor;
-uniform vec3  x3d_emissiveColor;
-uniform float x3d_shininess;
-uniform float x3d_transparency;
+uniform float x3d_AmbientIntensity;
+uniform vec3  x3d_DiffuseColor;
+uniform vec3  x3d_SpecularColor;
+uniform vec3  x3d_EmissiveColor;
+uniform float x3d_Shininess;
+uniform float x3d_Transparency;
 
-uniform bool      x3d_texturing; // true if a X3DTexture2DNode is attached, otherwise false
-uniform sampler2D x3d_texture;
+uniform bool      x3d_Texturing; // true if a X3DTexture2DNode is attached, otherwise false
+uniform sampler2D x3d_Texture;
 
 varying vec4 C;  // color
 varying vec4 t;  // texCoord
@@ -38,13 +38,13 @@ varying vec3 v;  // point on geometry
 vec4
 getTextureColor ()
 {
-	return texture2D (x3d_texture, vec2 (t .s, t .t));
+	return texture2D (x3d_Texture, vec2 (t .s, t .t));
 }
 
 void
 main ()
 {
-	if (x3d_lighting)
+	if (x3d_Lighting)
 	{
 		vec3  N  = normalize (gl_FrontFacing ? vN : -vN);
 		vec3  V  = normalize (-v); // normalized vector from point on geometry to viewer's position
@@ -53,11 +53,11 @@ main ()
 		// Calculate diffuseFactor & alpha
 
 		vec3  diffuseFactor = vec3 (1.0, 1.0, 1.0);
-		float alpha         = 1.0 - x3d_transparency;
+		float alpha         = 1.0 - x3d_Transparency;
 
-		if (x3d_colorMaterial)
+		if (x3d_ColorMaterial)
 		{
-			if (x3d_texturing)
+			if (x3d_Texturing)
 			{
 				vec4 T = getTextureColor ();
 
@@ -71,18 +71,18 @@ main ()
 		}
 		else
 		{
-			if (x3d_texturing)
+			if (x3d_Texturing)
 			{
 				vec4 T = getTextureColor ();
 
-				diffuseFactor  = T .rgb * x3d_diffuseColor;
+				diffuseFactor  = T .rgb * x3d_DiffuseColor;
 				alpha         *= T .a;
 			}
 			else
-				diffuseFactor = x3d_diffuseColor;
+				diffuseFactor = x3d_DiffuseColor;
 		}
 
-		vec3 ambientTerm = diffuseFactor * x3d_ambientIntensity;
+		vec3 ambientTerm = diffuseFactor * x3d_AmbientIntensity;
 
 		// Apply light sources
 
@@ -90,40 +90,40 @@ main ()
 
 		for (int i = 0; i < MAX_LIGHTS; ++ i)
 		{
-			float dL = length (x3d_lightLocation [i] - v);
+			float dL = length (x3d_LightLocation [i] - v);
 
-			if (x3d_lightOn [i] && (x3d_lightType [i] == DIRECTIONAL_LIGHT || dL <= x3d_lightRadius [i]))
+			if (x3d_LightOn [i] && (x3d_LightType [i] == DIRECTIONAL_LIGHT || dL <= x3d_LightRadius [i]))
 			{
-				vec3 c = x3d_lightAttenuation [i];
-				vec3 L = x3d_lightType [i] == DIRECTIONAL_LIGHT ? -x3d_lightDirection [i] : normalize (x3d_lightLocation [i] - v);
+				vec3 c = x3d_LightAttenuation [i];
+				vec3 L = x3d_LightType [i] == DIRECTIONAL_LIGHT ? -x3d_LightDirection [i] : normalize (x3d_LightLocation [i] - v);
 				vec3 H = normalize (L + V); // specular term
 
 				vec3  diffuseTerm    = diffuseFactor * max (dot (N, L), 0.0);
-				float specularFactor = bool (x3d_shininess) ? pow (max (dot (N, H), 0.0), x3d_shininess) : 1.0;
-				vec3  specularTerm   = x3d_specularColor * specularFactor;
+				float specularFactor = bool (x3d_Shininess) ? pow (max (dot (N, H), 0.0), x3d_Shininess) : 1.0;
+				vec3  specularTerm   = x3d_SpecularColor * specularFactor;
 
 				float attenuation = 1.0 / max (c [0] + c [1] * dL + c [2] * (dL * dL), 1.0);
 				float spot        = 1.0;
 
-				if (x3d_lightType [i] == SPOT_LIGHT)
+				if (x3d_LightType [i] == SPOT_LIGHT)
 				{
-					float spotAngle = acos (dot (-L, x3d_lightDirection [i]));
+					float spotAngle = acos (dot (-L, x3d_LightDirection [i]));
 					
-					if (spotAngle >= x3d_lightCutOffAngle [i])
+					if (spotAngle >= x3d_LightCutOffAngle [i])
 						spot = 0.0;
-					else if (spotAngle <= x3d_lightBeamWidth [i])
+					else if (spotAngle <= x3d_LightBeamWidth [i])
 						spot = 1.0;
 					else
-						spot = (spotAngle - x3d_lightCutOffAngle [i]) / (x3d_lightBeamWidth [i] - x3d_lightCutOffAngle [i]);
+						spot = (spotAngle - x3d_LightCutOffAngle [i]) / (x3d_LightBeamWidth [i] - x3d_LightCutOffAngle [i]);
 				}
 
-				finalColor += (attenuation * spot) * x3d_lightColor [i] *
-				              (x3d_lightAmbientIntensity [i] * ambientTerm +
-				               x3d_lightIntensity [i] * (diffuseTerm + specularTerm));
+				finalColor += (attenuation * spot) * x3d_LightColor [i] *
+				              (x3d_LightAmbientIntensity [i] * ambientTerm +
+				               x3d_LightIntensity [i] * (diffuseTerm + specularTerm));
 			}
 		}
 
-		finalColor += x3d_emissiveColor;
+		finalColor += x3d_EmissiveColor;
 
 		gl_FragColor = vec4 (finalColor, alpha);
 	}
@@ -131,9 +131,9 @@ main ()
 	{
 		vec4 finalColor = vec4 (1.0, 1.0, 1.0, 1.0);
 	
-		if (x3d_colorMaterial)
+		if (x3d_ColorMaterial)
 		{
-			if (x3d_texturing)
+			if (x3d_Texturing)
 			{
 				vec4 T = getTextureColor ();
 
@@ -144,7 +144,7 @@ main ()
 		}
 		else
 		{
-			if (x3d_texturing)
+			if (x3d_Texturing)
 				finalColor = getTextureColor ();
 		}
 
