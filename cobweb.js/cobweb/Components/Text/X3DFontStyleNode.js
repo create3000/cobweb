f201cd455260a1fa5d159099c9a1fa80ccce7172
Holger Seelig <holger.seelig@yahoo.de>
2015-09-20 09:53:51 +0200

    Updated cobweb.min.js.

--- cobweb.js/cobweb/Components/Text/X3DFontStyleNode.js
+++ cobweb.js/cobweb/Components/Text/X3DFontStyleNode.js
@@ -229,8 +229,7 @@ function ($,
 		},
 		setError: function (error)
 		{
-			console .warn ("Font error:", error);
-			console .warn (this .family [this .familyIndex]);
+			console .warn ('Font error: ' + error + ': "' + this .family [this .familyIndex] + '"');
 
 			this .font = null;
 			this .familyIndex ++;
