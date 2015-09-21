afda86e0fcd02a16ad95e9bb8b95ea751b1b8efa
Holger Seelig <holger.seelig@yahoo.de>
2015-09-21 16:39:47 +0200

    Update cobweb.min.js.

--- cobweb.js/cobweb.js
+++ cobweb.js/cobweb.js
@@ -1,38 +1,47 @@
 
-// Temporary X3D before page load.
-if (window .X3D === undefined)
+(function ()
 {
-	window .X3D = function X3D (callback, fallback)
+	// Temporary X3D before page load.
+	if (window .X3D === undefined)
 	{
-		X3D .callbacks .push (callback);
-		X3D .fallbacks .push (fallback);
-	};
-
-	X3D .callbacks = [ ];
-	X3D .fallbacks = [ ];
-
-	require (["cobweb/X3D"],
-	function (X3D)
-	{
-		var
-			callbacks = window .X3D .callbacks,
-			fallbacks = window .X3D .fallbacks;
-
-		window .X3D = X3D; // Now assign real X3D.
-
-		X3D (); // Initialize all X3D tags
-
-		for (var i = 0; i < callbacks .length; ++ i)
-		   X3D (callbacks [i], fallbacks [i]);
-	},
-	function (error)
-	{
-		require (["cobweb/Error"],
-		function (Error)
+		function X3D (callback, fallback)
 		{
-			Error (error, window .X3D .fallbacks);
+			X3D .callbacks .push (callback);
+			X3D .fallbacks .push (fallback);
+		}
 
-			delete window .X3D;
-		});
-	});
-}
+		X3D .callbacks = [ ];
+		X3D .fallbacks = [ ];
+		window .X3D    = X3D;
+
+		function fallback (error)
+		{
+			require (["cobweb/Error"],
+			function (Error)
+			{
+				Error (error, window .X3D .fallbacks);
+
+				delete window .X3D;
+			});
+		}
+
+		if (window .Proxy === undefined)
+		   return fallback ("Proxy is not defined");
+
+		require (["cobweb/X3D"],
+		function (X3D)
+		{
+			var
+				callbacks = window .X3D .callbacks,
+				fallbacks = window .X3D .fallbacks;
+
+			window .X3D = X3D; // Now assign real X3D.
+
+			X3D (); // Initialize all X3D tags
+
+			for (var i = 0; i < callbacks .length; ++ i)
+			   X3D (callbacks [i], fallbacks [i]);
+		},
+		fallback);
+	}
+}) ();
