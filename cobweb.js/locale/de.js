fb6a2c5d4e54908ef4002db18f462631796651f1
Holger Seelig <holger.seelig@yahoo.de>
2015-09-28 11:58:32 +0200

    Updated .fr.js

--- cobweb.js/locale/de.js
+++ cobweb.js/locale/de.js
@@ -3,7 +3,9 @@ define (function ()
 {
 	return {
 		"Cobweb X3D Browser": "Cobweb X3D Browser",
+
 		"Viewpoints": "Ansichtspunkte",
+
 		"Available Viewers": "Verfügbare Betrachter",
 		"Examine Viewer": "Untersuchen",
 		"Walk Viewer": "Laufen",
@@ -11,26 +13,32 @@ define (function ()
 		"Plane Viewer": "Ebenen Betrachter",
 		"None Viewer": "Kein Betrachter",
 		"Look At": "Auf Objekte zielen",
+
 		"Primitive Quality": "Qualität der Grundobjekte",
 		"Texture Quality": "Textur Qualität",
 		"Rubberband": "Gummiband",
 		"Display Rubberband": "Gummiband anzeigen",
 		"Browser Timings": "Zeitberechnung",
+
 		"Mute Browser": "Browser stumm schalten",
 		"Browser muted": "Browser stumm geschalten",
 		"Browser unmuted": "Browser Ton an",
 		"About Cobweb": "Über Cobweb",
+
 		"High": "Hoch",
 		"Medium": "Mittel",
 		"Low": "Niedrig",
 		"high": "hoch",
 		"medium": "mittel",
 		"low": "niedrig",
+
 		"on": "an",
 		"off": "aus",
+
 		"Loading %d files": "Lade %d Datei",
 		"Loading %d file": "Lade %d Dateien",
 		"Loading done": "Fertig mit Laden",
+
 		"More Properties": "Mehr Eigenschaften",
 		"Less Properties": "Weniger Eigenschaften",
 		"Frame rate": "Bildrate",
--- cobweb.js/locale/fr.js
+++ cobweb.js/locale/fr.js
@@ -3,25 +3,26 @@ define (function ()
 {
 	return {
 		"Cobweb X3D Browser": "Cobweb X3D Navigateur",
-		"Viewpoints": "Points de Vue",
+
+		"Viewpoints": "Points de vue",
 
 		"Available Viewers": "Visionneurs disponibles",
 		"Examine Viewer": "Examiner",
 		"Walk Viewer": "Aller",
 		"Fly Viewer": "Voler",
-		"Plane Viewer": "Visionneur de la Plaine",
-		"None Viewer": "pas de Visionneur",
-		"Look At": "Regarder un Objet de près",
+		"Plane Viewer": "Visionneur de la plaine",
+		"None Viewer": "Pas de visionneur",
+		"Look At": "Regarder un objet de près",
 
-		"Primitive Quality": "Qualité des Objets simples",
-		"Texture Quality": "Qualité des Textures",
+		"Primitive Quality": "Qualité des objets simples",
+		"Texture Quality": "Qualité des textures",
 		"Rubberband": "Bande élastique",
-		"Display Rubberband": "Présenter le Bande élastique",
-		"Browser Timings": "Calcul du Temps",
+		"Display Rubberband": "Présenter le bande élastique",
+		"Browser Timings": "Calcul du temps",
 
-		"Mute Browser": "Rendre Navigateur muet",
+		"Mute Browser": "Rendre navigateur muet",
 		"Browser muted": "Navigateur muet",
-		"Browser unmuted": "Son de Navigateur réactivé",
+		"Browser unmuted": "Son de navigateur réactivé",
 		"About Cobweb": "À propos de Cobweb",
 
 		"High": "Haut",
@@ -31,14 +32,15 @@ define (function ()
 		"medium": "moyenne",
 		"low": "faible",
 
-		"on": "marche", // ???
-		"off": "arrêt", // ???
+		"on": "marche",
+		"off": "arrêt",
 	
 		"Loading %d files": "Télécharger %d fichiers",
 		"Loading %d file": "Télécharger %d fichier",
 		"Loading done": "Téléchargement fini",
-		"More Properties": "Plus de Propriétés",
-		"Less Properties": "Moins de Propriétés",
+
+		"More Properties": "Plus de propriétés",
+		"Less Properties": "Moins de propriétés",
 		"Frame rate": "Fréquence",
 		"Speed": "Vélocité",
 		"Browser": "Navigateur",
