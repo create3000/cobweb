f8ae633ca4bf95eb11a385fd21526d96e1cfdb6d
Holger Seelig <holger.seelig@yahoo.de>
2015-11-18 05:14:33 +0100

    Gettext

--- cobweb.js/lib/gettext.js
+++ cobweb.js/lib/gettext.js
@@ -7,12 +7,6 @@ function (de, fr)
 {
 "use strict";
 
-	var locales =
-	{
-		de: de,
-		fr: fr,
-	};
-
 	function execAll (regex, string)
 	{
 		var
@@ -25,26 +19,50 @@ function (de, fr)
 		return matches;
 	}
 
-	var msg = /msgid\s+"(.*?)"\nmsgstr\s+"(.*?)"\n/g;
-
-	for (var lang in locales)
+	function getLanguage ()
 	{
-		var
-			matches = execAll (msg, locales [lang]),
-			locale  = locales [lang] = { };
-
-		for (var i = 0, length = matches .length; i < length; ++ i)
+		for (var i = 0; i < navigator .languages; ++ i)
 		{
-			if (matches [i] [2] .length)
-				locale [matches [i] [1]] = matches [i] [2];
+			var language = navigator .languages [i] .split ("-") [0];
+	
+			if (locales [language])
+				return language;
+		}
+
+		return (navigator .language || navigator .userLanguage) .split ("-") [0];
+	}
+
+	function setLocale (language)
+	{
+		if (locales [language])
+		{
+			var
+				matches = execAll (msg, locales [language]),
+				locale  = locales [language] = { };
+	
+			for (var i = 0, length = matches .length; i < length; ++ i)
+			{
+				if (matches [i] [2] .length)
+					locale [matches [i] [1]] = matches [i] [2];
+			}
 		}
 	}
 
+	var locales =
+	{
+		de: de,
+		fr: fr,
+	};
+
+	var
+		msg      = /msgid\s+"(.*?)"\nmsgstr\s+"(.*?)"\n/g,
+		language = getLanguage ();
+
+	setLocale (language);
+
 	function gettext (string)
 	{
-		var
-			language = (navigator .language || navigator .userLanguage) .split ("-") [0],
-			ĺocale   = locales [language];
+		var ĺocale = locales [language];
 
 		if (ĺocale === undefined)
 			return string;
