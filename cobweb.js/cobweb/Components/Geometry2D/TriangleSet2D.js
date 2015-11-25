a1a3b06961724670c2ea757f71b3312ea52c97c9
Holger Seelig <holger.seelig@yahoo.de>
2015-11-25 19:32:05 +0100

    Implemented TriangleSet2D.

--- cobweb.js/cobweb/Components/Geometry2D/TriangleSet2D.js
+++ cobweb.js/cobweb/Components/Geometry2D/TriangleSet2D.js
@@ -6,15 +6,21 @@ define ([
 	"cobweb/Basic/FieldDefinitionArray",
 	"cobweb/Components/Rendering/X3DGeometryNode",
 	"cobweb/Bits/X3DConstants",
+	"standard/Math/Numbers/Vector3",
 ],
 function ($,
           Fields,
           X3DFieldDefinition,
           FieldDefinitionArray,
           X3DGeometryNode, 
-          X3DConstants)
+          X3DConstants,
+          Vector3)
 {
 "use strict";
+
+	var
+		normal = new Vector3 (0, 0, 1),
+		vector = new Vector3 (0, 0, 0);
 
 	function TriangleSet2D (executionContext)
 	{
@@ -42,6 +48,47 @@ function ($,
 		getContainerField: function ()
 		{
 			return "geometry";
+		},
+		build: function ()
+		{
+			var vertices = this .vertices_ .getValue ();
+
+			for (var i = 0, length = vertices .length; i < length; ++ i)
+			{
+				var vertex = vertices [i];
+
+				this .addNormal (normal);
+				this .addVertex (vector .set (vertex .x, vertex .y, 0));
+			}
+
+			this .setSolid (this .solid_ .getValue ());
+		},
+		buildTexCoords: function ()
+		{
+			var
+				p         = this .getTexCoordParams (),
+				min       = p .min,
+				Ssize     = p .Ssize,
+				texCoords = [ ],
+				vertices  = this .vertices;
+
+			this .texCoords .push (texCoords);
+
+			for (var i = 0, length = this .vertices .length; i < length; i += 4)
+			{
+				texCoords .push ((vertices [i]     - min [0]) / Ssize,
+				                 (vertices [i + 1] - min [1]) / Ssize,
+				                 0,
+				                 1);
+			}
+		},
+		traverse: function (context)
+		{
+			context .geometryType = 2;
+
+			X3DGeometryNode .prototype .traverse .call (this, context);
+
+			context .geometryType = 3;
 		},
 	});
 
