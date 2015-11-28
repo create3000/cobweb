60093e9a3b43482a2bdf7b98519701134d8b73c9
Holger Seelig <holger.seelig@yahoo.de>
2015-11-28 18:21:12 +0100

    Implemented TriangleStripSet.

--- cobweb.js/cobweb/Components/Rendering/TriangleStripSet.js
+++ cobweb.js/cobweb/Components/Rendering/TriangleStripSet.js
@@ -21,6 +21,8 @@ function ($,
 		X3DComposedGeometryNode .call (this, executionContext .getBrowser (), executionContext);
 
 		this .addType (X3DConstants .TriangleStripSet);
+
+		this .triangleIndex = [ ];
 	}
 
 	TriangleStripSet .prototype = $.extend (Object .create (X3DComposedGeometryNode .prototype),
@@ -51,6 +53,48 @@ function ($,
 		getContainerField: function ()
 		{
 			return "geometry";
+		},
+		initialize ()
+		{
+			X3DComposedGeometryNode .prototype .initialize .call (this);
+		
+			this .stripCount_ .addInterest (this, "set_stripCount__");
+		
+			this .set_stripCount__ ();
+		},
+		set_stripCount__: function ()
+		{
+			// Build coordIndex
+
+			var
+				stripCount    = this .stripCount_ .getValue (),
+				triangleIndex = this .triangleIndex;
+
+			triangleIndex .length = 0;
+
+			for (var s = 0, strips = stripCount .length, index = 0; s < strips; ++ s)
+			{
+				var vertexCount = stripCount [s] .getValue ()
+
+				for (var i = 0, count = vertexCount - 2; i < count; ++ i)
+				{
+					var is_odd = i & 1;
+
+					triangleIndex .push (index + (is_odd ? i + 1 : i),
+					                     index + (is_odd ? i : i + 1),
+					                     index + (i + 2));
+				}
+
+				index += vertexCount;
+			}
+		},
+		getPolygonIndex: function (index)
+		{
+			return this .triangleIndex [index];
+		},
+		build: function ()
+		{
+			X3DComposedGeometryNode .prototype .build .call (this, 3, this .triangleIndex .length, 3, this .triangleIndex .length);
 		},
 	});
 
