db367b4d2067a52aecc37b03a718b39a3cbcf19d
Holger Seelig <holger.seelig@yahoo.de>
2015-11-28 18:01:24 +0100

    Implemented TriangleFanSet.

--- cobweb.js/cobweb/Components/Rendering/TriangleFanSet.js
+++ cobweb.js/cobweb/Components/Rendering/TriangleFanSet.js
@@ -21,6 +21,8 @@ function ($,
 		X3DComposedGeometryNode .call (this, executionContext .getBrowser (), executionContext);
 
 		this .addType (X3DConstants .TriangleFanSet);
+
+		this .triangleIndex = [ ];
 	}
 
 	TriangleFanSet .prototype = $.extend (Object .create (X3DComposedGeometryNode .prototype),
@@ -51,6 +53,44 @@ function ($,
 		getContainerField: function ()
 		{
 			return "geometry";
+		},
+		initialize ()
+		{
+			X3DComposedGeometryNode .prototype .initialize .call (this);
+		
+			this .fanCount_ .addInterest (this, "set_fanCount__");
+		
+			this .set_fanCount__ ();
+		},
+		set_fanCount__: function ()
+		{
+			// Build coordIndex
+
+			var
+				fanCount      = this .fanCount_ .getValue (),
+				triangleIndex = this .triangleIndex;
+		
+			triangleIndex .length = 0;
+
+			for (var f = 0, fans = fanCount .length, index = 0; f < fans; ++ f)
+			{
+				var vertexCount = fanCount [f] .getValue ()
+
+				for (var i = 1, count = vertexCount - 1; i < count; ++ i)
+				{
+					triangleIndex .push (index, index + i, index + i + 1);
+				}
+		
+				index += vertexCount;
+			}
+		},
+		getPolygonIndex: function (index)
+		{
+			return this .triangleIndex [index];
+		},
+		build: function ()
+		{
+			X3DComposedGeometryNode .prototype .build .call (this, 3, this .triangleIndex .length, 3, this .triangleIndex .length);
 		},
 	});
 
