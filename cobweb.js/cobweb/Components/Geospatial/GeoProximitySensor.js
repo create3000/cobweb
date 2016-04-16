14eaa8dd00d520421989449fb8d35a24a99cf6d5
Holger Seelig <holger.seelig@yahoo.de>
2016-04-16 07:34:53 +0200

    Implemented GeoProximitySensor.

--- cobweb.js/cobweb/Components/Geospatial/GeoProximitySensor.js
+++ cobweb.js/cobweb/Components/Geospatial/GeoProximitySensor.js
@@ -6,17 +6,23 @@ define ([
 	"cobweb/Basic/FieldDefinitionArray",
 	"cobweb/Components/EnvironmentalSensor/X3DEnvironmentalSensorNode",
 	"cobweb/Components/Geospatial/X3DGeospatialObject",
+	"cobweb/Components/EnvironmentalSensor/ProximitySensor",
 	"cobweb/Bits/X3DConstants",
+	"standard/Math/Numbers/Vector3",
 ],
 function ($,
           Fields,
           X3DFieldDefinition,
           FieldDefinitionArray,
           X3DEnvironmentalSensorNode, 
-          X3DGeospatialObject, 
-          X3DConstants)
+          X3DGeospatialObject,
+          ProximitySensor,
+          X3DConstants,
+          Vector3)
 {
 "use strict";
+
+	var geoCoord = new Vector3 (0, 0, 0);
 
 	function GeoProximitySensor (executionContext)
 	{
@@ -24,6 +30,10 @@ function ($,
 		X3DGeospatialObject        .call (this, executionContext);
 
 		this .addType (X3DConstants .GeoProximitySensor);
+
+		this .proximitySensor = new ProximitySensor (executionContext);
+
+		this .setCameraObject (this .proximitySensor .getCameraObject ());
 	}
 
 	GeoProximitySensor .prototype = $.extend (Object .create (X3DEnvironmentalSensorNode .prototype),
@@ -61,6 +71,35 @@ function ($,
 		{
 			X3DEnvironmentalSensorNode .prototype .initialize .call (this);
 			X3DGeospatialObject        .prototype .initialize .call (this);
+
+			this .enabled_ .addFieldInterest (this .proximitySensor .enabled_);
+			this .size_    .addFieldInterest (this .proximitySensor .size_);
+			this .center_  .addFieldInterest (this .proximitySensor .center_);
+		
+			this .proximitySensor .isCameraObject_ .addFieldInterest (this .isCameraObject_);
+		
+			this .proximitySensor .isActive_                 .addFieldInterest (this .isActive_);
+			this .proximitySensor .enterTime_                .addFieldInterest (this .enterTime_);
+			this .proximitySensor .exitTime_                 .addFieldInterest (this .exitTime_);
+			this .proximitySensor .position_changed_         .addFieldInterest (this .position_changed_);
+			this .proximitySensor .orientation_changed_      .addFieldInterest (this .orientation_changed_);
+			this .proximitySensor .centerOfRotation_changed_ .addFieldInterest (this .centerOfRotation_changed_);
+		
+			this .proximitySensor .position_changed_ .addInterest (this, "set_position__");
+		
+			this .proximitySensor .enabled_ = this .enabled_;
+			this .proximitySensor .size_    = this .size_;
+			this .proximitySensor .center_  = this .center_;
+		
+			this .proximitySensor .setup ();
+		},
+		set_position__ (position)
+		{
+			this .geoCoord_changed_ = this .getGeoCoord (this .proximitySensor .position_changed_ .getValue (), geoCoord);
+		},
+		traverse (type)
+		{
+			this .proximitySensor .traverse (type);
 		},
 	});
 
