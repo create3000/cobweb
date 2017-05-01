16d4e58fe82e7595564fb2bfeaadb89feba6dcf2
Holger Seelig <holger.seelig@yahoo.de>
2017-05-01 13:49:02 +0200

    Added cobweb.zip.

--- cobweb.js/version.pl
+++ cobweb.js/version.pl
@@ -6,15 +6,20 @@ use warnings;
 use v5.10.0;
 use open qw/:std :utf8/;
 
-say `pwd`;
+my $CWD = `pwd`;
+chomp $CWD;
+
+say $CWD;
 
 my $VERSION = `cat cobweb/Browser/VERSION.js`;
 $VERSION =~ /"(.*?)"/;
 $VERSION = $1;
 
 my $MAJOR       = int ($VERSION);
-my $VERSION_DIR = "../stable/$MAJOR/$VERSION";
-my $LATEST      = "../stable/latest";
+my $STABLE_DIR  = "../stable";
+my $MAJOR_DIR   = "$STABLE_DIR/$MAJOR";
+my $VERSION_DIR = "$MAJOR_DIR/$VERSION";
+my $LATEST      = "$STABLE_DIR/latest";
 
 sub cp {
 	my $VERSION_DIR = shift;
@@ -31,9 +36,23 @@ sub cp {
 	system "perl", "-pi", "-e", 's|\s*<script src="jam/require\.js"></script>\n||sg', "$VERSION_DIR/browser.html";
 }
 
+sub zip {
+	my $ZIP_DIR = "cobweb-$VERSION";
+
+	chdir $MAJOR_DIR;
+	system "mv", $VERSION, $ZIP_DIR;
+
+	system "zip", "-r", "$ZIP_DIR.zip", $ZIP_DIR;
+	system "mv", "-v", "$ZIP_DIR.zip", $ZIP_DIR;
+
+	system "mv", $ZIP_DIR, $VERSION;
+	chdir $CWD;
+}
+
 exit unless -d $VERSION_DIR;
 
 say "Making version '$VERSION' now.";
 
 cp $VERSION_DIR;
 cp $LATEST;
+zip;
